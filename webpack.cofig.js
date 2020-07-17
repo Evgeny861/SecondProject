2f01a2757dd64de3c06e95525a8122dda410b4aa
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-07-17 23:55:39 +0300

    Промежуточный вариант

--- webpack.cofig.js
+++ webpack.cofig.js
@@ -1,13 +1,15 @@
-const path = require('path')
+/* eslint-disable no-undef */
+// eslint-disable-next-line strict
+const path = require('path');
 
 module.exports = {
     entry: {
         main: './src/index.js'
     },
     output: {
+        // eslint-disable-next-line no-undef
         path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
         publicPath: '/dist'
-        
     }
-}
+};
