9365ddab22d8b7172b83a9d761288a8a5a4ea794
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-07-20 20:46:25 +0300

    Сделал скролл из 19

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import sendForm from './modules/sendForm';
 import 'formdata-polyfill';
 
 // Timer
-countTimer('21 july 2020');
+countTimer('24 july 2020');
 // Menu
 toggleMenu();
 // popup
--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,8 +1,9 @@
 const toggleMenu = () => {
-
     const menu = document.querySelector('menu'),
         closeBtn = document.querySelector('.close-btn'),
-        body = document.querySelector('body');
+        body = document.querySelector('body'),
+        linkNavs = menu.querySelectorAll('[href^="#"]'),
+        serviceBlock = document.querySelector(['[href^="#service-block"]']);
 
     const handlerMenu = () => {
         menu.classList.toggle('active-menu');
@@ -16,8 +17,26 @@ const toggleMenu = () => {
             } else if (menu.classList.contains('active-menu') && target !== menu && !target.matches('li')) {
                 handlerMenu();
             }
+    });
 
+    for (const linkNav of linkNavs) {
+        linkNav.addEventListener('click', event => {
+            event.preventDefault();
+            const blockID = linkNav.getAttribute('href');
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        });
+    }
 
+    serviceBlock.addEventListener('click', event => {
+        event.preventDefault();
+        const blockID = serviceBlock.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
     });
 
 
